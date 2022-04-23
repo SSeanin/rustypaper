6fbe911122b2ec69362465072be924adabd7df8a
Saeed Andalib <saeedandalib5@gmail.com>
2022-04-23 11:31:37 +0430

    feat: domain post structure

--- src/lib/domain/post.rs
+++ src/lib/domain/post.rs
@@ -1,1 +1,15 @@
 pub mod field;
+
+use field::{Content, CreatedAt, IsPublished, PostId, Shortcode, Title, UpdatedAt};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Post {
+    pub post_id: PostId,
+    pub title: Title,
+    pub content: Content,
+    pub shortcode: Shortcode,
+    pub is_published: IsPublished,
+    pub created_at: CreatedAt,
+    pub updated_at: UpdatedAt,
+}
--- src/lib/domain/post/field.rs
+++ src/lib/domain/post/field.rs
@@ -1,12 +1,20 @@
 use crate::domain::validation::ValidationError;
 
-pub mod post_id;
-pub mod title;
 pub mod content;
-pub mod shortcode;
-pub mod is_published;
 pub mod created_at;
+pub mod is_published;
+pub mod post_id;
+pub mod shortcode;
+pub mod title;
 pub mod updated_at;
+
+pub use content::Content;
+pub use created_at::CreatedAt;
+pub use is_published::IsPublished;
+pub use post_id::PostId;
+pub use shortcode::Shortcode;
+pub use title::Title;
+pub use updated_at::UpdatedAt;
 
 pub(self) type Result<T> = std::result::Result<T, FieldError>;
 
@@ -15,5 +23,5 @@ pub enum FieldError {
     #[error("field validation failed: {0}")]
     Validation(#[from] ValidationError),
     #[error("invalid boolean value: {0}")]
-    ParseBool(#[from] std::str::ParseBoolError)
+    ParseBool(#[from] std::str::ParseBoolError),
 }
