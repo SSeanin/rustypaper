789647cd96c6de495e070344e2e5085a31c798dc
Saeed Andalib <saeedandalib5@gmail.com>
2022-06-30 20:02:53 +0430

    hashing the password in password fields new function

--- src/lib/domain/user/field/password.rs
+++ src/lib/domain/user/field/password.rs
@@ -1,16 +1,24 @@
 use crate::domain::Result;
 use argon2::password_hash::{rand_core::OsRng, SaltString};
 use argon2::{Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
+use derive_more::From;
 use serde::{Deserialize, Serialize};
 use std::borrow::Cow;
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, From)]
 pub struct Password(String);
 
 impl Password {
     pub fn new(password: String) -> Result<Self> {
         match validator::validate_length(&password, Some(8), None, None) {
-            true => Ok(Self(password)),
+            true => {
+                let salt = SaltString::generate(&mut OsRng);
+                Ok(Self(
+                    Argon2::default()
+                        .hash_password(password.as_bytes(), &salt)?
+                        .to_string(),
+                ))
+            }
             false => {
                 let mut error = validator::ValidationError::new("length");
                 error.message = Some(Cow::from("Password must be longer than 8 characters"));
@@ -20,16 +28,8 @@ impl Password {
         }
     }
 
-    pub fn to_hashed(&self) -> Result<String> {
-        let salt = SaltString::generate(&mut OsRng);
-        Ok(Argon2::default()
-            .hash_password(self.0.as_bytes(), &salt)?
-            .to_string())
-    }
-
-    pub fn verify(password: &str, hash: &str) -> Result<()> {
-        let parsed_hash = PasswordHash::new(hash)?;
-        Ok(Argon2::default().verify_password(password.as_bytes(), &parsed_hash)?)
+    pub fn verify(&self, password: &str) -> Result<()> {
+        Ok(Argon2::default().verify_password(password.as_bytes(), &PasswordHash::new(&self.0)?)?)
     }
 
     pub fn into_inner(self) -> String {
