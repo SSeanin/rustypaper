cb20eae5e232d2da38b90a941df56274db071285
Saeed Andalib <saeedandalib5@gmail.com>
2022-07-01 11:41:53 +0430

    chore: updated initial database migration to create the user table

--- migrations/20220422102756_initial.up.sql
+++ migrations/20220422102756_initial.up.sql
@@ -1,5 +1,5 @@
 -- Add up migration script here
-CREATE TABLE IF NOT EXISTS post
+CREATE TABLE IF NOT EXISTS "post"
 (
     post_id      uuid UNIQUE              NOT NULL DEFAULT gen_random_uuid(),
     title        VARCHAR(255)             NOT NULL,
@@ -9,4 +9,16 @@ CREATE TABLE IF NOT EXISTS post
     created_at   TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at   TIMESTAMP WITH TIME ZONE NOT NULL,
     PRIMARY KEY (post_id)
+);
+
+CREATE TABLE IF NOT EXISTS "user"
+(
+    user_id         uuid UNIQUE              NOT NULL DEFAULT gen_random_uuid(),
+    first_name      VARCHAR(255)             NOT NULL,
+    last_name       VARCHAR(255)             NOT NULL,
+    email           VARCHAR (255)            NOT NULL UNIQUE,
+    password        VARCHAR (2048)           NOT NULL,
+    created_at      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP WITH TIME ZONE NOT NULL,
+    PRIMARY KEY (user_id)
 );
